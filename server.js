e9f282ee55e6ce17a4bd239d2184d74950a5de16
Cooeny <Emanuelgraciano678@gmail.com>
2026-02-05 16:27:35 -0400

    fix: install chromium on deploy

--- server.js
+++ server.js
@@ -116,5 +116,5 @@ app.get("/stj/informativo", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 app.listen(PORT, () => console.log("Servidor rodando na porta", PORT));
